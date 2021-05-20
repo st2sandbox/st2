a76a1b7f8c8a1beeba9630e5b0f2e7bbf336db67
Jacob Floyd <cognifloyd@gmail.com>
2021-05-19 20:02:41 -0500

    pants: add stevedore_extensions rule to get extra kwargs for setup.py

--- pants-plugins/stevedore_extensions/register.py
+++ pants-plugins/stevedore_extensions/register.py
@@ -3,12 +3,14 @@
 from pants.backend.codegen import export_codegen_goal
 
 from stevedore_extensions import (
-    target_types_rules, rules as stevedore_rules, pytest_dependency_injection
+    target_types_rules,
+    rules as stevedore_rules,
+    pytest_dependency_injection,
+    setup_py_kwargs
 )
 from stevedore_extensions.target_types import StevedoreExtension
 
 
-# TODO: add the entry_points automatically to setup_py
 # TODO: add stevedore_namespaces field to python_library?
 
 
@@ -18,6 +20,7 @@ def rules():
         *stevedore_rules.rules(),
         *pytest_dependency_injection.rules(),
         *export_codegen_goal.rules(),
+        *setup_py_kwargs.rules(),
     ]
 
 
---
+++ pants-plugins/stevedore_extensions/setup_py_kwargs.py
@@ -0,0 +1,90 @@
+# coding: utf-8
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import List, Tuple
+
+from pants.backend.python.goals.setup_py import SetupKwargsRequest
+from pants.base.specs import AddressSpecs, SiblingAddresses
+from pants.engine.addresses import Address
+from pants.engine.rules import collect_rules, Get, MultiGet, rule
+from pants.engine.target import Targets
+from pants.util.frozendict import FrozenDict
+
+from stevedore_extensions.target_types import (
+    ResolvedStevedoreEntryPoints,
+    ResolveStevedoreEntryPointsRequest,
+    StevedoreEntryPoints,
+    StevedoreEntryPointsField,
+    StevedoreExtension,
+    StevedoreNamespaceField,
+)
+
+
+@dataclass(frozen=True)
+class StevedoreSetupKwargs:
+    kwargs: FrozenDict[str, FrozenDict[str, Tuple[str]]]
+
+
+@dataclass(frozen=True)
+class StevedoreSetupKwargsRequest:
+    """
+    Light wrapper around SetupKwargsRequest to allow composed Kwargs.
+
+    Only one plugin can provide Kwargs for a given setup, so that repo-specific
+    plugin should do something like this:
+
+    custom_args = {...}
+    stevedore_kwargs = await Get(StevedoreSetupKwargs, StevedoreSetupKwargsRequest(request))
+    return SetupKwargs(
+        **request.explicit_kwargs,
+        **stevedore_kwargs.kwargs,
+        **custom_args,
+        address=address
+    )
+    """
+    request: SetupKwargsRequest
+
+
+@rule
+async def stevedore_kwargs_for_setup_py(
+    stevedore_request: StevedoreSetupKwargsRequest
+) -> StevedoreSetupKwargs:
+    request: SetupKwargsRequest = stevedore_request.request
+    address: Address = request.target.address
+
+    sibling_targets = await Get(
+        Targets, AddressSpecs([SiblingAddresses(address.spec_path)]),
+    )
+    stevedore_targets: List[StevedoreExtension] = [
+        tgt for tgt in sibling_targets if tgt.has_field(StevedoreEntryPointsField)
+    ]
+    resolved_entry_points: Tuple[ResolvedStevedoreEntryPoints, ...] = await MultiGet(
+        Get(
+            ResolvedStevedoreEntryPoints,
+            ResolveStevedoreEntryPointsRequest(tgt[StevedoreEntryPointsField])
+        )
+        for tgt in stevedore_targets
+    )
+
+    entry_points_kwargs = defaultdict(list)
+    for target, resolved_ep in zip(stevedore_targets, resolved_entry_points):
+        namespace: StevedoreNamespaceField = target[StevedoreNamespaceField]
+        entry_points: StevedoreEntryPoints = resolved_ep.val
+
+        for entry_point in entry_points:
+            entry_points_kwargs[namespace].append(
+                f"{entry_point.name} = {entry_point.value.spec}\n"
+            )
+    return StevedoreSetupKwargs(
+        FrozenDict(
+            {
+                "entry_points": FrozenDict(
+                    (k, tuple(v)) for k, v in sorted(entry_points_kwargs)
+                )
+            }
+        )
+    )
+
+
+def rules():
+    return collect_rules()
