7aa9b6b5b4f5d532b9d112a14107f35e2ca10e35
Jacob Floyd <cognifloyd@gmail.com>
2021-06-01 21:17:21 -0500

    Rename fixtureloader func to be more generic

--- st2tests/st2tests/fixtures/packs/dummy_pack_7/fixture.py
+++ st2tests/st2tests/fixtures/packs/dummy_pack_7/fixture.py
@@ -1,4 +1,4 @@
 from st2tests import fixturesloader
 
 PACK_NAME = "dummy_pack_7_name"
-PACK_DIR_NAME, PACK_PATH = fixturesloader.get_pack_fixture_name_and_path_from(__file__)
+PACK_DIR_NAME, PACK_PATH = fixturesloader.get_fixture_name_and_path(__file__)
