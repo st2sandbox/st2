e9ba5bcafdfa502c7bffd303ab9b962bf24b03df
Jacob Floyd <cognifloyd@gmail.com>
2021-06-02 00:05:59 -0500

    Import PATH from st2tests/fixtures/runners fixture
    Let pants know that these runners are in use for these tests

--- st2common/tests/integration/test_register_content_script.py
+++ st2common/tests/integration/test_register_content_script.py
@@ -25,6 +25,7 @@ from st2tests import config as test_config
 from st2tests.fixturesloader import get_fixtures_packs_base_path
 from st2tests.fixtures.packs.dummy_pack_1.fixture import PACK_PATH as DUMMY_PACK_1_PATH
 from st2tests.fixtures.packs.dummy_pack_4.fixture import PACK_PATH as DUMMY_PACK_4_PATH
+from st2tests.fixtures.packs.runners.fixture import FIXTURE_PATH as RUNNER_DIRS
 from st2tests.fixtures.packs_1.dummy_pack_4.fixture import PACK_PATH as EMPTY_PACK_PATH
 
 
@@ -47,7 +48,7 @@ class ContentRegisterScriptTestCase(IntegrationTestCase):
 
     def test_register_from_pack_success(self):
         pack_dir = DUMMY_PACK_1_PATH
-        runner_dirs = os.path.join(get_fixtures_packs_base_path(), "runners")
+        runner_dirs = RUNNER_DIRS
 
         opts = [
             "--register-pack=%s" % (pack_dir),
@@ -61,7 +62,7 @@ class ContentRegisterScriptTestCase(IntegrationTestCase):
     def test_register_from_pack_fail_on_failure_pack_dir_doesnt_exist(self):
         # No fail on failure flag, should succeed
         pack_dir = "doesntexistblah"
-        runner_dirs = os.path.join(get_fixtures_packs_base_path(), "runners")
+        runner_dirs = RUNNER_DIRS
 
         opts = [
             "--register-pack=%s" % (pack_dir),
@@ -86,7 +87,7 @@ class ContentRegisterScriptTestCase(IntegrationTestCase):
     def test_register_from_pack_action_metadata_fails_validation(self):
         # No fail on failure flag, should succeed
         pack_dir = DUMMY_PACK_4_PATH
-        runner_dirs = os.path.join(get_fixtures_packs_base_path(), "runners")
+        runner_dirs = RUNNER_DIRS
 
         opts = [
             "--register-pack=%s" % (pack_dir),
---
+++ st2tests/st2tests/fixtures/packs/runners/fixture.py
@@ -0,0 +1,3 @@
+from st2tests import fixturesloader
+
+FIXTURE_NAME, FIXTURE_PATH = fixturesloader.get_fixture_name_and_path(__file__)
