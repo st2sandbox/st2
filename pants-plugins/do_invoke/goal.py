dc3544b8f13e6f3001d09f9b8db328da83d0bf06
Jacob Floyd <cognifloyd@gmail.com>
2021-07-31 11:46:05 -0500

    flesh out basics of running invoke

--- pants-plugins/do_invoke/goal.py
+++ pants-plugins/do_invoke/goal.py
@@ -12,14 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pants.backend.python.target_types import EntryPoint
+from pants.backend.python.util_rules.pex import (
+    InterpreterConstraints,
+    Pex,
+    PexProcess,
+    PexRequest,
+    PexRequirements,
+)
+from pants.engine.fs import AddPrefix, Digest, EMPTY_DIGEST, Workspace
 from pants.engine.goal import Goal, GoalSubsystem
-from pants.engine.process import InteractiveRunner, InteractiveProcess
-from pants.engine.rules import collect_rules, goal_rule
+from pants.engine.process import InteractiveRunner, InteractiveProcess, Process
+from pants.engine.rules import collect_rules, Get, goal_rule, MultiGet
 
 
 class DoInvokeSubsystem(GoalSubsystem):
     name = "do"
     help = "Run pyinvoke-based targets (locally; remote execution not supported)"
+    # TODO: dynamic help pulled from invoke
 
 
 class DoInvoke(Goal):
@@ -31,12 +41,33 @@ class DoInvoke(Goal):
 #    - or in a "tasks" module (organized into collections/namespaces of tasks)
 
 @goal_rule
-async def do_invoke(interactive_runner: InteractiveRunner) -> DoInvoke:
-    # TODO: get/install invoke
+async def do_invoke(interactive_runner: InteractiveRunner, workspace: Workspace) -> DoInvoke:
+    invoke_pex = await Get(
+        Pex,
+        PexRequest(
+            output_filename="invoke.pex",
+            internal_only=True,
+            requirements=PexRequirements(["invoke==1.6.0"]),
+            interpreter_constraints=InterpreterConstraints(["CPython>=3.6"]),
+            main=EntryPoint.parse("invoke.main:program.run"),
+        )
+    )
+    invoke_pex_digest, invoke_pex_process = await MultiGet(
+        Get(Digest, AddPrefix(invoke_pex.digest, ".pex/")),
+        Get(Process, PexProcess(invoke_pex, description="Invoke")),
+    )
+    workspace.write_digest(invoke_pex_digest)
+
+    invoke_argv = [".pex/invoke.pex" if arg == "invoke.pex" else arg for arg in invoke_pex_process.argv]
+
+    invoke_argv.extend(["--help"])
+    print(invoke_argv)
+
     result = interactive_runner.run(
         InteractiveProcess(
-            argv=["invoke"],
-            env={},
+            argv=invoke_argv,
+            env=invoke_pex_process.env,
+            input_digest=EMPTY_DIGEST, # run_in_workspace requires EMPTY_DIGEST
             run_in_workspace=True,
             forward_signals_to_process=True,
         )
